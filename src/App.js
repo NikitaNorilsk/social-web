0a95df48150803aed6cd5cb885ae15713e444ff5
NikitaNorilsk <break24@mail.ru>
2023-01-18 00:21:03 +0300

    test commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className='max-w-[1200px] mx-auto'>
+      <Header />
+      <h1></h1>
+      <div className='flex pt-3'>
+        <div className="w-1/5">
+          <Nav />
+        </div>
+        <div className="w-4/5 pl-3">
+          <Profile />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
