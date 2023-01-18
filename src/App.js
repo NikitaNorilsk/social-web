0d6946c03c4cf2ad38261fc64836faeca504e37c
NikitaNorilsk <break24@mail.ru>
2023-01-19 00:54:49 +0300

    19.01.23 0:54 before sleep

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './components/Header';
+import MyPosts from './components/MyPosts';
 import Nav from './components/Nav';
+import Post from './components/Post';
 import Profile from './components/Profile';
 
 function App() {
@@ -9,12 +11,15 @@ function App() {
       <Header />
       <h1></h1>
       <div className='flex pt-3'>
-        Flash
         <div className="w-1/5">
           <Nav />
         </div>
         <div className="w-4/5 pl-3">
           <Profile />
+          <MyPosts />
+          <Post name="Жора Зубенко" message='Всем здравствуйте, это мой первый пост' img='https://i.picsum.photos/id/52/200/200.jpg?hmac=CQCpnfTd-I1A6rOsdB9t4Dt3W3nllS8VW1azyfj8phY' />
+          <Post name="Алексей Ишудченко" message='Продолжаем.' img='https://i.picsum.photos/id/666/200/200.jpg?hmac=keuaJ7nJC5hkHIEyee1b6jdEMbHBZbozYQ9NClt2_lk' />
+          <Post name="Андрей Щербин" message='Не плохо получается, только не останавливайся' img='https://i.picsum.photos/id/692/200/200.jpg?hmac=PBAPILlS_qaKe0OMYjT0eXnftJjTGdwOgqCQMUVDGbg' />
         </div>
       </div>
     </div>
