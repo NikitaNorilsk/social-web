4df7e6e77e296cdc998dc0d7c025891e6395ec73
NikitaNorilsk <break24@mail.ru>
2023-01-27 20:50:51 +0300

    trying add MUI

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { addPostActionCreator, updateNewPostTextActionCreator } from './Data'
 
 
-
 export default function MyPosts(props) {
 
     let newPostElement = React.createRef()
@@ -10,9 +9,9 @@ export default function MyPosts(props) {
         let text = newPostElement.current.value
         // props.dispatch({ type: 'ADD-POST', postMessage: text })
         props.dispatch(addPostActionCreator(text))
-    } 
+    }
 
-    const onPostChange = () => { 
+    const onPostChange = () => {
         let text = newPostElement.current.value
         // props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text})
         props.dispatch(updateNewPostTextActionCreator(text))
@@ -24,11 +23,11 @@ export default function MyPosts(props) {
             <div className="w-24 h-20 bg-gray-300">
                 <button>Remove</button>
                 <input
-                value={props.data.newPostText}
-                ref={newPostElement}
-                onChange={onPostChange}
-                type="text"
-                className='bg-gray-100' />
+                    value={props.data.newPostText}
+                    ref={newPostElement}
+                    onChange={onPostChange}
+                    type="text"
+                    className='bg-gray-100' />
                 <button onClick={addNew}>Add post</button>
             </div>
         </div>
